2d3e460d965d7c22e26a169247c44684fdf536cc
unknown <nppot.winas@gmail.com>
2026-01-21 17:00:14 +0700

    4545

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,16 @@
 // Write your code below
+function printLeftHalfPyramid(): void {
+    let n = Number(process.argv.slice(2));
+    for (let i: number = 1; i <= n; i++) {
+        let row: string = '';
+        for (let j: number = i; j <= n-i; j++) {
+            row += ' ';
+        }
+        for (let k: number = i; k >= i; k++) {
+            row += k ;
+        }
+        console.log(row);
+    }
+}
+
+printLeftHalfPyramid();
