74241cd147e092ee62a6a8c92fa19a0b304b87d3
unknown <nppot.winas@gmail.com>
2026-01-21 16:11:47 +0700

    111

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,15 @@
 // Write your code below
+const a = Number(process.argv.slice(2));
+if(isNaN(a)|| a < 0 || a > 100 ){
+    console.log("Invalid input");
+}else if(a >= 80){
+    console.log("A");
+}else if( a >= 70 && a < 80){
+    console.log("B");
+}else if( a >= 60 && a < 70){
+    console.log("C");
+}else if( a >= 50 && a < 60){
+    console.log("D");
+}else{
+    console.log("F");
+}
